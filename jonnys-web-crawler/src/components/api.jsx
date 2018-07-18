436ed5282694af756d1ec3b036844c2de1a9641b
jamiemetca <jamiemetca@gmail.com>
2018-07-18 14:11:49 +0100

    edit api file to return false data

--- jonnys-web-crawler/src/components/api.jsx
+++ jonnys-web-crawler/src/components/api.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const checkWebsiteLinks = () => {
   return {
-    "https://broken-links-api.herokuapp.com/": [
+    urls: [
       { "https://web-crawler-test1.herokuapp.com/": true },
       { "https://web-crawler-test2.herokuapp.com/": false },
       { "https://random-static-linky-site.herokuapp.com/": true },
